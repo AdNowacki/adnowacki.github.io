8a05117d2fa1031bdbfb484ef94a12262bd8e37d
Adam Nowacki <adam@grochow.waw.pl>
2018-03-08 12:45:57 +0100

    Dodanie projektu TKN slider 20180308

---
+++ projekty/tkn-slider/js/config.js
@@ -0,0 +1,61 @@
+var CONFIG = CONFIG || {};
+
+CONFIG.userAgent = function()
+{
+    this.userAg;    // Nazwa przeglądarki
+    this.setBodyId = function(ctrl)
+    {
+        var nav = navigator.userAgent;
+        if (nav.indexOf('Firefox') != -1) {
+            this.userAg = 'firefox';
+        } else if (nav.indexOf('Opera') != -1) {
+            this.userAg = 'opera';
+        } else if (nav.indexOf('Chrome') != -1) {
+            this.userAg = 'chrome';
+        } else if (nav.indexOf('MSIE 10') != -1) {
+            this.userAg = 'ie10';
+        } else if (nav.indexOf('MSIE 9') != -1) {
+            this.userAg = 'ie9';
+        } else if (nav.indexOf('MSIE 8') != -1) {
+            this.userAg = 'ie8';
+        } else if (nav.indexOf('Safari') != -1) {
+            this.userAg = 'safari';
+        };
+        if (ctrl == true) {
+            document.body.setAttribute('id', this.userAg);
+        };
+    };
+};
+
+CONFIG.window = function()
+{
+    this.windowWidth = null, // Szerokość okna przeglądarki
+    this.windowHeight = null, // Wysokość okna przeglądarki
+    this.screenWidth = null, // Szerokość monitora
+    this.screenHeight = null, // Wysokość monitora
+
+    this.setWindow = function()
+    {
+        this.windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        this.windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        this.screenWidth = screen.width;
+        this.screenHeight = screen.height;
+    };
+}
+
+CONFIG.init = function()
+{
+    CONFIG.obStorage.window.setWindow();
+    CONFIG.obStorage.userAgent.setBodyId(true);
+}
+
+CONFIG.obStorage = {
+    window: new CONFIG.window(),
+    userAgent: new CONFIG.userAgent(),
+}
+
+
+
+$(document).ready(function() {
+    CONFIG.init();
+});
---
+++ projekty/tkn-slider/js/slider.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+
+    var time_ = 520;
+
+    $('#_s1').mouseenter(function(){
+        $('#_s1').animate({'width': "458px", "height" : "172px", "left" : "0", "top" : "0"}, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "512px", "height" : "172px", "top" : "0"}, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"width": "189px",'bottom' : "-61px", "height" : "228px", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"width": "389px", 'bottom' : "-61px", "height" : "228px", "left":"199px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "598px", "bottom": "58px", "height" : "109px", "width":"191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "58px", "height" : "109px", "width" : "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "598px", "bottom": "-60px", "height" : "108px","width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "-60px", "height" : "108px", "width": "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+
+
+
+    $('#_s2').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "172px", "left" : "-100px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "682px", "height" : "172px", "top" : "0"}, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"width": "189px",'bottom' : "-61px", "height" : "228px", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"width": "389px", 'bottom' : "-61px", "height" : "228px", "left":"199px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "598px", "bottom": "58px", "height" : "109px", "width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "58px", "height" : "109px", "width" : "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "598px", "bottom": "-60px", "height" : "108px","width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "-60px", "height" : "108px", "width": "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+    $('#_s3').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "-70px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "-70px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "296px", "width":"388px", "bottom":"0px", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"width": "391px", "height" : "296px","left" : "398px", "bottom":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "799px", "bottom": "141px", "height" : "155px", "width": "193px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "-208px", "bottom": "141px", "height" : "157px", "width" : "184px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "799px", "bottom": "-26px", "height" : "157px", "width": "193px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "-208px", "bottom": "0", "height" : "131px", "width" : "184px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+    $('#_s4').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "-70px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "-70px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "296px", "width":"189px", "bottom" : "0", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "296px","left" : "199px", "width" : "531px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "740px", "bottom": "141px", "height" : "155px", "width": "193px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "-147px", "bottom": "141px", "height" : "155px", "width" : "184px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "740px", "bottom": "-26px", "height" : "157px", "width": "193px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "-156px", "bottom": "-26px", "height" : "157px", "width": "193px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+    $('#_s5').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "226px", "width":"189px", "bottom" : "0", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "226px","left" : "199px", "width" : "391px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "600px", "bottom": "121px", "height" : "105px", "width" : "253px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "-67px", "bottom": "121px", "height" : "105px", "width" : "184px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "600px", "bottom": "0", "height" : "111px", "width": "253px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "-67px", "bottom": "0", "height" : "111px", "width": "184px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+
+    $('#_s6').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "226px", "width":"189px", "bottom" : "0", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "226px","left" : "199px", "width" : "389px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "598px", "bottom": "124px", "height" : "102px", "width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "66px", "height" : "160px", "width" : "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "598px", "bottom": "0", "height" : "114px", "width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "-59px", "height" : "115px", "width": "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+    $('#_s7').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "-40px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "-40px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "266px", "width":"189px", "bottom" : "0", "left" : "-40px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "266px","left" : "159px", "width" : "387px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "556px", "bottom": "161px", "height" : "105px", "width": "233px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "121px", "height" : "145px", "width" : "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "556px", "bottom": "0", "height" : "151px", "width": "233px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "0", "height" : "111px", "width": "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+    $('#_s8').mouseenter(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0px", "top" : "-40px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "-40px" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "266px", "width":"189px", "bottom" : "0", "left" : "-40px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "266px","left" : "159px", "width" : "387px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "556px", "bottom": "164px", "height" : "102px", "width": "190px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "164px", "height" : "102px", "width": "224px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "556px", "bottom": "0", "height" : "154px", "width": "190px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "0", "height" : "154px", "width": "224px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+    });
+
+
+
+    $("._slider").mouseleave(function(){
+        $('#_s1').animate({'width': "388px", "height" : "110px", "left" : "0", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s2').animate({'width': "582px", "height" : "110px", "top" : "0" }, {duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s3').animate({"height" : "226px", "width":"189px", "bottom" : "0", "left":"0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s4').animate({"height" : "226px","left" : "199px", "width" : "389px", "bottom" : "0"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s5').animate({"left" : "598px", "bottom": "124px", "height" : "102px", "width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s6').animate({"right" : "0", "bottom": "124px", "height" : "102px", "width":"181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s7').animate({"left" : "598px", "bottom": "0", "height" : "114px", "width": "191px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('#_s8').animate({"right" : "0", "bottom": "0", "height" : "114px", "width": "181px"},{duration: time_, queue: false, easing : "easeInOutSine"});
+        $('.color').fadeOut(time_,"easeInOutSine");
+        $('.gray').fadeIn(time_,"easeInOutSine");
+    });
+
+
+    $("._sliderImgs").mouseenter(function(){
+        $('.color').fadeOut(time_,"easeInOutSine");
+        $('.gray').fadeIn(time_,"easeInOutSine");
+        $(this).find(".gray").fadeOut(time_,"easeInOutSine");
+        $(this).find(".color").fadeIn(time_,"easeInOutSine");
+    })
+
+
+});

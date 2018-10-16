858846923c47f8dc750c34809fb57d350b22919b
Adam Nowacki <adam@grochow.waw.pl>
2018-10-16 22:46:47 +0200

    Orange Roaming

---
+++ projekty/roaming/js/x-markers.js
@@ -0,0 +1,827 @@
+var xMarkersIcon = {
+  '1': 'marker-1.png',
+  '2': 'marker-2.png',
+  '3': 'marker-3.png',
+  // '4': 'marker-4.png',
+  '4': 'marker-3.png',
+}
+
+var xInfoStatus = {
+  '1': '<span class="x-status x-status--1">Zachowaj (zwykłą) ostrożność</span>',
+  '2': '<span class="x-status x-status--2">Zachowaj szczególną ostrożność</span>',
+  '3': '<span class="x-status x-status--3">MSZ odradza podróże, które nie są konieczne</span>',
+  '4': '<span class="x-status x-status--4">MSZ odradza wszelkie podróże</span>',
+}
+
+var xInfoStatusDescription = {
+  '1': 'Poziom zagrożenia zbliżony do Polski.',
+  '2': 'Poziom zagrożenia wyższy niż w Polsce.',
+  '3': 'Poziom zagrożenia znacznie wyższy niż w Polsce. W naszej ocenie podróże konieczne to podróże w celach zawodowych, pilnych sprawach rodzinnych itd. Podróży urlopowych nie uważamy za konieczne. Ostateczna decyzja, czy podróż jest konieczna należy do obywatela.',
+  '4': 'Najwyższy poziom zagrożenia. Odradzane są wszelkie podróże, w tym w celach zawodowych i pilnych sprawach rodzinnych.',
+}
+
+var xMarkers = {
+  '1': {
+    position: {lat: 33.93911, lng: 67.709953},
+    name: 'Afganistan',		
+  },
+  '2': {
+    position: {lat: 41.153332, lng: 20.168331},
+    name: 'Albania',	
+  },
+  '3': {
+    position: {lat: 28.033886, lng: 1.659626},
+    name: 'Algieria',	 	
+  },
+  '4': {
+    position: {lat: 42.546245, lng: 1.601554},
+    name: 'Andora',	 	
+  },
+  '5': {
+    position: {lat: -11.202692, lng: 17.873887},
+    name: 'Angola',	 	
+  },
+  '6': {
+    position: {lat: 17.060816, lng: -61.796428},
+    name: 'Antigua i Barbuda',	 	
+  },
+  '7': {
+    position: {lat: 23.885942, lng: 45.079162},
+    name: 'Arabia Saudyjska',	 	
+  },
+  '8': {
+    position: {lat: -38.416097, lng: -63.616672},
+    name: 'Argentyna',	 	
+  },
+  '9': {
+    position: {lat: 40.069099, lng: 45.038189},
+    name: 'Armenia',	 	
+  },
+  '10': {
+    position: {lat: -25.274398, lng: 133.775136},
+    name: 'Australia',	 	
+  },
+  '11': {
+    position: {lat: 47.516231, lng: 14.550072},
+    name: 'Austria',	 	
+  },
+  '12': {
+    position: {lat: 40.143105, lng: 47.576927},
+    name: 'Azerbejdżan',	 	
+  },
+  '13': {
+    position: {lat: 25.03428, lng: -77.39628},
+    name: 'Bahamy',	 	
+  },
+  '14': {
+    position: {lat: 25.930414, lng: 50.637772},
+    name: 'Bahrajn',	 	
+  },
+  '15': {
+    position: {lat: 23.684994, lng: 90.356331},
+    name: 'Bangladesz',	 	
+  },
+  '16': {
+    position: {lat: 13.193887, lng: -59.543198},
+    name: 'Barbados',	 	
+  },
+  '17': {
+    position: {lat: 50.503887, lng: 4.469936},
+    name: 'Belgia',	 	
+  },
+  '18': {
+    position: {lat: 17.189877, lng: -88.49765},
+    name: 'Belize',	 	
+  },
+  '19': {
+    position: {lat: 9.30769, lng: 2.315834},
+    name: 'Benin',	 	
+  },
+  '20': {
+    position: {lat: 27.514162, lng: 90.433601},
+    name: 'Bhutan',	 	
+  },
+  '21': {
+    position: {lat: 53.709807, lng: 27.953389},
+    name: 'Białoruś',	 	
+  },
+  '22': {
+    position: {lat: -16.290154, lng: -63.588653},
+    name: 'Boliwia',	 	
+  },
+  '23': {
+    position: {lat: 43.915886, lng: 17.679076},
+    name: 'Bośnia i Hercegowina',	 	
+  },
+  '24': {
+    position: {lat:  	-22.328474, lng: 24.684866},
+    name: 'Botswana',	 	
+  },
+  '25': {
+    position: {lat: -14.235004, lng: -51.92528},
+    name: 'Brazylia',	 	
+  },
+  '26': {
+    position: {lat: 4.535277, lng: 114.727669},
+    name: 'Brunei',	 	
+  },
+  '27': {
+    position: {lat: 42.733883, lng: 25.48583},
+    name: 'Bułgaria',	 	
+  },
+  '28': {
+    position: {lat:  	12.238333, lng: -1.561593},
+    name: 'Burkina Faso',	 	
+  },
+  '29': {
+    position: {lat: -3.373056, lng: 29.918886},
+    name: 'Burundi',	 	
+  },
+  '30': {
+    position: {lat: -35.675147, lng: -71.542969},
+    name: 'Chile',	 	
+  },
+  '31': {
+    position: {lat: 35.86166, lng: 104.195397},
+    name: 'Chińska Republika Ludowa',	 	
+  },
+  '32': {
+    position: {lat:  	45.1, lng: 15.2},
+    name: 'Chorwacja',	 	
+  },
+  '33': {
+    position: {lat: 35.126413, lng: 33.429859},
+    name: 'Cypr',	 	
+  },
+  '34': {
+    position: {lat: 15.454166, lng: 18.732207},
+    name: 'Czad',	 	
+  },
+  '35': {
+    position: {lat: 42.708678, lng: 19.37439},
+    name: 'Czarnogóra',	 	
+  },
+  '36': {
+    position: {lat:  	49.817492, lng: 15.472962},
+    name: 'Czechy',	 	
+  },
+  '37': {
+    position: {lat: 56.26392, lng: 9.501785},
+    name: 'Dania',	 	
+  },
+  '38': {
+    position: {lat: -4.038333, lng: 21.758664},
+    name: 'Kongo',	 	
+  },
+  '39': {
+    position: {lat: 15.414999, lng: -61.370976},
+    name: 'Dominika',	 	
+  },
+  '40': {
+    position: {lat: 18.735693, lng: -70.162651},
+    name: 'Dominikana',	 	
+  },
+  '41': {
+    position: {lat: 11.825138, lng: 42.590275},
+    name: 'Dżibuti',	 	
+  },
+  '42': {
+    position: {lat: 26.820553, lng: 30.802498},
+    name: 'Egipt',	 	
+  },
+  '43': {
+    position: {lat:  	-1.831239, lng: -78.183406},
+    name: 'Ekwador',	 	
+  },
+  '44': {
+    position: {lat: 15.179384, lng: 39.782334},
+    name: 'Erytrea',	 	
+  },
+  '45': {
+    position: {lat: 58.595272, lng: 25.013607},
+    name: 'Estonia',	 	
+  },
+  '46': {
+    position: {lat: 9.145, lng: 40.489673},
+    name: 'Etiopia',	 	
+  },
+  '47': {
+    position: {lat: -16.578193, lng: 179.414413},
+    name: 'Fidżi',	 	
+  },
+  '48': {
+    position: {lat: 12.879721, lng: 121.774017},
+    name: 'Filipiny',	 	
+  },
+  '49': {
+    position: {lat: 61.92411, lng: 25.748151},
+    name: 'Finlandia',	 	
+  },
+  '50': {
+    position: {lat:  	46.227638, lng: 2.213749},
+    name: 'Francja',	 	
+  },
+  '51': {
+    position: {lat: -0.803689, lng: 11.609444},
+    name: 'Gabon',	 	
+  },
+  '52': {
+    position: {lat: 13.443182, lng: -15.310139},
+    name: 'Gambia',	 	
+  },
+  '53': {
+    position: {lat: 7.946527, lng: -1.023194},
+    name: 'Ghana',	 	
+  },
+  '54': {
+    position: {lat: 39.074208, lng: 21.824312},
+    name: 'Grecja',	 	
+  },
+  '55': {
+    position: {lat: 12.062776, lng: -61.714171},
+    name: 'Grenada',	 	
+  },
+  '56': {
+    position: {lat: 42.315407, lng: 43.356892},
+    name: 'Gruzja',	 	
+  },
+  '57': {
+    position: {lat: 3.733889, lng: -59.125782},
+    name: 'Gujana',	 	
+  },
+  '58': {
+    position: {lat: 15.783471, lng: -90.230759},
+    name: 'Gwatemala',	 	
+  },
+  '59': {
+    position: {lat:  	9.945587, lng: -9.696645},
+    name: 'Gwinea',	 	
+  },
+  '60': {
+    position: {lat: 11.803749, lng: -15.180413},
+    name: 'Gwinea Bissau',	 	
+  },
+  '61': {
+    position: {lat: -11.202692, lng: 17.873887},
+    name: 'Gwinea Równikowa',	 	
+  },
+  '62': {
+    position: {lat: 18.971187, lng: -72.285215},
+    name: 'Haiti',	 	
+  },
+  '63': {
+    position: {lat:  	40.463667, lng: -3.74922},
+    name: 'Hiszpania',	 	
+  },
+  '64': {
+    position: {lat: 52.132633, lng: 5.291266},
+    name: 'Holandia',	 	
+  },
+  '65': {
+    position: {lat: 15.199999, lng: -86.241905},
+    name: 'Honduras',	 	
+  },
+  '66': {
+    position: {lat: 22.396428, lng: 114.109497},
+    name: 'Hong Kong',
+    noExist: true,
+  },
+  '67': {
+    position: {lat: 20.593684, lng: 78.96288},
+    name: 'Indie',	 	
+  },
+  '68': {
+    position: {lat: -0.789275, lng: 113.921327},
+    name: 'Indonezja',	 	
+  },
+  '69': {
+    position: {lat: 33.223191, lng: 43.679291},
+    name: 'Irak',	 	
+  },
+  '70': {
+    position: {lat: 32.427908, lng: 53.688046},
+    name: 'Iran',	 	
+  },
+  '71': {
+    position: {lat: 53.41291, lng: -8.24389},
+    name: 'Irlandia',	 	
+  },
+  '72': {
+    position: {lat: 64.963051, lng: -19.020835},
+    name: 'Islandia',	 	
+  },
+  '73': {
+    position: {lat: 31.046051, lng: 34.851612},
+    name: 'Izrael',	 	
+  },
+  '74': {
+    position: {lat: 18.109581, lng: -77.297508},
+    name: 'Jamajka',	 	
+  },
+  '75': {
+    position: {lat: 36.204824, lng: 138.252924},
+    name: 'Japonia',	 	
+  },
+  '76': {
+    position: {lat: 15.552727, lng: 48.516388},
+    name: 'Jemen',	 	
+  },
+  '77': {
+    position: {lat: 30.585164, lng: 36.238414},
+    name: 'Jordania',	 	
+  },
+  '78': {
+    position: {lat: 12.565679, lng: 104.990963},
+    name: 'Kambodża',	 	
+  },
+  '79': {
+    position: {lat: 7.369722, lng: 12.354722},
+    name: 'Kamerun',	 	
+  },
+  '80': {
+    position: {lat: 56.130366, lng: -106.346771},
+    name: 'Kanada',	 	
+  },
+  '81': {
+    position: {lat: 25.354826, lng: 51.183884},
+    name: 'Katar',	 	
+  },
+  '82': {
+    position: {lat: 48.019573, lng: 66.923684},
+    name: 'Kazachstan',	 	
+  },
+  '83': {
+    position: {lat: -0.023559, lng: 37.906193},
+    name: 'Kenia',	 	
+  },
+  '84': {
+    position: {lat: 41.20438, lng: 74.766098},
+    name: 'Kirgistan',	 	
+  },
+  '85': {
+    position: {lat: -3.370417, lng: -168.734039},
+    name: 'Kiribati',	 	
+    noExist: true,
+  },
+  '86': {
+    position: {lat: 4.570868, lng: -74.297333},
+    name: 'Kolumbia',	 	
+  },
+  '87': {
+    position: {lat: -11.875001, lng: 43.872219},
+    name: 'Komory',	 	
+  },
+  '88': {
+    position: {lat: -0.228021, lng: 15.827659},
+    name: 'Kongo',	 	
+  },
+  '89': {
+    position: {lat: 35.907757, lng: 127.766922},
+    name: 'Korea Południowa',	 	
+  },
+  '90': {
+    position: {lat: 40.339852, lng: 127.510093},
+    name: 'Korea Północna',	 	
+  },
+  '91': {
+    position: {lat: 42.602636, lng: 20.902977},
+    name: 'Kosowo',	 	
+  },
+  '92': {
+    position: {lat: 9.748917, lng: -83.753428},
+    name: 'Kostaryka',	 	
+  },
+  '93': {
+    position: {lat: 21.521757, lng: -77.781167},
+    name: 'Kuba',	 	
+  },
+  '94': {
+    position: {lat: 29.31166, lng: 47.481766},
+    name: 'Kuwejt',	 	
+  },
+  '95': {
+    position: {lat: 19.85627, lng: 102.495496},
+    name: 'Laos',	 	
+  },
+  '96': {
+    position: {lat: -29.609988, lng: 28.233608},
+    name: 'Lesotho',	 	
+  },
+  '97': {
+    position: {lat: 33.854721, lng: 35.862285},
+    name: 'Liban',	 	
+  },
+  '98': {
+    position: {lat: 6.428055, lng: -9.429499},
+    name: 'Liberia',	 	
+  },
+  '99': {
+    position: {lat: 26.3351, lng: 17.228331},
+    name: 'Libia',	 	
+  },
+  '100': {
+    position: {lat: 47.166, lng: 9.555373},
+    name: 'Liechtenstein',	 	
+  },
+  '101': {
+    position: {lat: 55.169438, lng: 23.881275},
+    name: 'Litwa',	 	
+  },
+  '102': {
+    position: {lat: 49.815273, lng: 6.129583},
+    name: 'Luksemburg',	 	
+  },
+  '103': {
+    position: {lat: 56.879635, lng: 24.603189},
+    name: 'Łotwa',	 	
+  },
+  '104': {
+    position: {lat: 41.608635, lng: 21.745275},
+    name: 'Macedonia',	 	
+  },
+  '105': {
+    position: {lat: -18.766947, lng: 46.869107},
+    name: 'Madagaskar',	 	
+  },
+  '106': {
+    position: {lat: 22.198745, lng: 113.543873},
+    name: 'Makau',	 	
+  },
+  '107': {
+    position: {lat: -13.254308, lng: 34.301525},
+    name: 'Malawi',	 	
+  },
+  '108': {
+    position: {lat: 3.202778, lng: 73.22068},
+    name: 'Malediwy',	 	
+  },
+  '109': {
+    position: {lat: 4.210484, lng: 101.975766},
+    name: 'Malezja',	 	
+  },
+  '110': {
+    position: {lat: 17.570692, lng: -3.996166},
+    name: 'Mali',	 	
+  },
+  '111': {
+    position: {lat: 35.937496, lng: 14.375416},
+    name: 'Malta',	 	
+  },
+  '112': {
+    position: {lat: 31.791702, lng: -7.09262},
+    name: 'Maroko',	 	
+  },
+  '113': {
+    position: {lat:  	21.00789, lng: -10.940835},
+    name: 'Mauretania',	 	
+  },
+  '114': {
+    position: {lat: -20.348404, lng: 57.552152},
+    name: 'Mauritius',	 	
+  },
+  '115': {
+    position: {lat: 23.634501, lng: -102.552784},
+    name: 'Meksyk',	 	
+  },
+  '116': {
+    position: {lat: 5.925554, lng: 160.550812},
+    name: 'Mikronezja',	 	
+  },
+  '117': {
+    position: {lat: 21.913965, lng: 95.956223},
+    name: 'Mjanma/Birma',	 
+    noExist: true,	
+  },
+  '118': {
+    position: {lat: 47.411631, lng: 28.369885},
+    name: 'Mołdawia',	 	
+  },
+  '119': {
+    position: {lat: 43.750298, lng: 7.412841},
+    name: 'Monako',	 	
+  },
+  '120': {
+    position: {lat: 46.862496, lng: 103.846656},
+    name: 'Mongolia',	 	
+  },
+  '121': {
+    position: {lat: -18.665695, lng: 35.529562},
+    name: 'Mozambik',	 	
+  },
+  '122': {
+    position: {lat: -22.95764, lng: 18.49041},
+    name: 'Namibia',	 	
+  },
+  '123': {
+    position: {lat: -0.522778, lng: 166.931503},
+    name: 'Nauru',	 	
+    noExist: true,
+  },
+  '124': {
+    position: {lat: 28.394857, lng: 84.124008},
+    name: 'Nepal',	 	
+  },
+  '125': {
+    position: {lat: 51.165691, lng: 10.451526},
+    name: 'Niemcy',	 	
+  },
+  '126': {
+    position: {lat: 17.607789, lng: 8.081666},
+    name: 'Niger',	 	
+  },
+  '127': {
+    position: {lat: 9.081999, lng: 8.675277},
+    name: 'Nigeria',	 	
+  },
+  '128': {
+    position: {lat: 12.865416, lng: -85.207229},
+    name: 'Nikaragua',	 	
+  },
+  '129': {
+    position: {lat: 60.472024, lng: 8.468946},
+    name: 'Norwegia',	 	
+  },
+  '130': {
+    position: {lat: -40.900557, lng: 174.885971},
+    name: 'Nowa Zelandia',	 	
+  },
+  '131': {
+    position: {lat: 21.512583, lng: 55.923255},
+    name: 'Oman',	 	
+  },
+  '132': {
+    position: {lat: 30.375321, lng: 69.345116},
+    name: 'Pakistan',	 	
+  },
+  '133': {
+    position: {lat: 7.51498, lng: 134.58252},
+    name: 'Palau',	 	
+    noExist: true,
+  },
+  '134': {
+    position: {lat: 8.537981, lng: -80.782127},
+    name: 'Panama',	 	
+  },
+  '135': {
+    position: {lat: -6.314993, lng: 143.95555},
+    name: 'Papua Nowa Gwinea',	 	
+  },
+  '136': {
+    position: {lat:  	-23.442503, lng: -58.443832},
+    name: 'Paragwaj',	 	
+  },
+  '137': {
+    position: {lat: -9.189967, lng: -75.015152},
+    name: 'Peru',	 	
+  },
+  '138': {
+    position: {lat: 39.399872, lng: -8.224454},
+    name: 'Portugalia',	 	
+  },
+  '139': {
+    position: {lat: -30.559482, lng: 22.937506},
+    name: 'Republika Południowej Afryki',	 	
+  },
+  '140': {
+    position: {lat: 6.611111, lng: 20.939444},
+    name: 'Republika Środkowoafrykańska',	 	
+  },
+  '141': {
+    position: {lat: 16.002082, lng: -24.013197},
+    name: 'Republika Zielonego Przylądka',	 	
+  },
+  '142': {
+    position: {lat: 61.52401, lng: 105.318756},
+    name: 'Rosja',	 	
+  },
+  '143': {
+    position: {lat: 45.943161, lng: 24.96676},
+    name: 'Rumunia',	 	
+  },
+  '144': {
+    position: {lat: -1.940278, lng: 29.873888},
+    name: 'Rwanda',	 	
+  },
+  '145': {
+    position: {lat: 17.357822, lng: -62.782998},
+    name: 'Saint Kitts and Nevis',	 	
+    noExist: true,
+  },
+  '146': {
+    position: {lat: 13.909444, lng: -60.978893},
+    name: 'Saint Lucia',	 	
+    noExist: true,
+  },
+  '147': {
+    position: {lat: 12.984305, lng: -61.287228},
+    name: 'Saint Vincent i Grenadyny',	 	
+    noExist: true,
+  },
+  '148': {
+    position: {lat: 13.794185, lng: -88.89653},
+    name: 'Salwador',	 	
+  },
+  '149': {
+    position: {lat: -13.759029, lng: -172.104629},
+    name: 'Samoa',	 	
+  },
+  '150': {
+    position: {lat: 43.94236, lng: 12.457777},
+    name: 'San Marino',	 	
+  },
+  '151': {
+    position: {lat: 14.497401, lng: -14.452362},
+    name: 'Senegal',	 	
+  },
+  '152': {
+    position: {lat:  	44.016521, lng: 21.005859},
+    name: 'Serbia',	 	
+  },
+  '153': {
+    position: {lat: -4.679574, lng: 55.491977},
+    name: 'Seszele',	 	
+  },
+  '154': {
+    position: {lat: 8.460555, lng: -11.779889},
+    name: 'Sierra Leone',	 	
+  },
+  '155': {
+    position: {lat: 1.352083, lng: 103.819836},
+    name: 'Singapur',	 	
+  },
+  '156': {
+    position: {lat: 48.669026, lng: 19.699024},
+    name: 'Słowacja',	 	
+  },
+  '157': {
+    position: {lat: 46.151241, lng: 14.995463},
+    name: 'Słowenia',	 	
+  },
+  '158': {
+    position: {lat: 5.152149, lng: 46.199616},
+    name: 'Somalia',	 	
+  },
+  '159': {
+    position: {lat: 7.873054, lng: 80.771797},
+    name: 'Sri Lanka',	 	
+  },
+  '160': {
+    position: {lat: 37.09024, lng: -95.712891},
+    name: 'Stany Zjednoczone',	 	
+  },
+  '161': {
+    position: {lat: -26.522503, lng: 31.465866},
+    name: 'Suazi',	 	
+  },
+  '162': {
+    position: {lat:  	12.862807, lng: 30.217636},
+    name: 'Sudan',	 	
+  },
+  '163': {
+    position: {lat: -11.202692, lng: 17.873887},
+    name: 'Sudan Południowy',	 	
+    noExist: true,
+  },
+  '164': {
+    position: {lat:  	3.919305, lng: -56.027783},
+    name: 'Surinam',	 	
+  },
+  '165': {
+    position: {lat:  	34.802075, lng: 38.996815},
+    name: 'Syria',	 	
+  },
+  '166': {
+    position: {lat: 46.818188, lng: 8.227512},
+    name: 'Szwajcaria',	 	
+  },
+  '167': {
+    position: {lat: 60.128161, lng: 18.643501},
+    name: 'Szwecja',	 	
+  },
+  '168': {
+    position: {lat: 38.861034, lng: 71.276093},
+    name: 'Tadżykistan',	 	
+  },
+  '169': {
+    position: {lat: 15.870032, lng: 100.992541},
+    name: 'Tajlandia',	 	
+  },
+  '170': {
+    position: {lat: 23.69781, lng: 120.960515},
+    name: 'Tajwan',	 	
+  },
+  '171': {
+    position: {lat: -6.369028, lng: 34.888822},
+    name: 'Tanzania',	 	
+  },
+  '172': {
+    position: {lat: -8.874217, lng: 125.727539},
+    name: 'Timor Wschodni',
+    noExist: true,
+  },
+  '173': {
+    position: {lat: 8.619543, lng: 0.824782},
+    name: 'Togo',	 	
+  },
+  '174': {
+    position: {lat: -21.178986, lng: -175.198242},
+    name: 'Tonga',	 	
+    noExist: true,
+  },
+  '175': {
+    position: {lat: 10.691803, lng: -61.222503},
+    name: 'Trynidad i Tobago',	 	
+  },
+  '176': {
+    position: {lat: 33.886917, lng: 9.537499},
+    name: 'Tunezja',	 	
+  },
+  '177': {
+    position: {lat: 38.963745, lng: 35.243322},
+    name: 'Turcja',	 	
+  },
+  '178': {
+    position: {lat: 38.969719, lng: 59.556278},
+    name: 'Turkmenistan',	 	
+  },
+  '179': {
+    position: {lat: -7.109535, lng: 177.64933},
+    name: 'Tuvalu',	 	
+    noExist: true,
+  },
+  '180': {
+    position: {lat: 1.373333, lng: 32.290275},
+    name: 'Uganda',	 	
+  },
+  '181': {
+    position: {lat:  	48.379433, lng: 31.16558},
+    name: 'Ukraina',	 	
+  },
+  '182': {
+    position: {lat: -32.522779, lng: -55.765835},
+    name: 'Urugwaj',	 	
+  },
+  '183': {
+    position: {lat: 41.377491, lng: 64.585262},
+    name: 'Uzbekistan',	 	
+  },
+  '184': {
+    position: {lat: -15.376706, lng: 166.959158},
+    name: 'Vanuatu',	 	
+  },
+  '185': {
+    position: {lat: 41.902916, lng: 12.453389},
+    name: 'Watykan',	 	
+    noExist: true,
+  },
+  '186': {
+    position: {lat: 6.42375, lng: -66.58973},
+    name: 'Wenezuela',	 	
+  },
+  '187': {
+    position: {lat: 47.162494, lng: 19.503304},
+    name: 'Węgry',	 	
+  },
+  '188': {
+    position: {lat: 55.378051, lng: -3.435973},
+    name: 'Wielka Brytania',	 	
+  },
+  '189': {
+    position: {lat: 14.058324, lng: 108.277199},
+    name: 'Wietnam',	 	
+  },
+  '190': {
+    position: {lat: 41.87194, lng: 12.56738},
+    name: 'Włochy',	 	
+  },
+  '191': {
+    position: {lat: 7.539989, lng: -5.54708},
+    name: 'Wybrzeże Kości Słoniowej',	 	
+  },
+  '192': {
+    position: {lat: 7.131474, lng: 171.184478},
+    name: 'Wyspy Marshalla',	 	
+  },
+  '193': {
+    position: {lat: -9.64571, lng: 160.156194},
+    name: 'Wyspy Salomona',	 	
+  },
+  '194': {
+    position: {lat: 0.18636, lng: 6.613081},
+    name: 'Wyspy Świętego Tomasza i Książęca',	 	
+  },
+  '195': {
+    position: {lat: -13.133897, lng: 27.849332},
+    name: 'Zambia',	 	
+  },
+  '196': {
+    position: {lat: -19.015438, lng: 29.154857},
+    name: 'Zimbabwe',	 	
+  },
+  '197': {
+    position: {lat: 23.424076, lng: 53.847818},
+    name: 'Zjednoczone Emiraty Arabskie',	 	
+  },
+}
+
+ 	
